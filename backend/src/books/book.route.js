5adc7986ef3a50e139f930597671ce4ff035df3c
Magartej <155970891+Magartej@users.noreply.github.com>
2025-05-17 17:24:09 +0545

    token

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const Book = require('./book.model');
 const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
 
-// const verifyAdminToken = require('../middleware/verifyAdminToken');
 const router = express.Router();
 
 // frontend => backend server => controller => book schema  => database => send to server => back to the frontend
@@ -12,7 +12,7 @@ const router = express.Router();
 // delete = when delete something
 
 // post a book
-router.post("/create-book", postABook)
+router.post("/create-book", verifyAdminToken, postABook)
 
 
 // get all books
@@ -22,9 +22,9 @@ router.get("/", getAllBooks);
 router.get("/:id", getSingleBook);
 
 // update a book endpoint
-router.put("/edit/:id", UpdateBook);
+router.put("/edit/:id",verifyAdminToken, UpdateBook);
 
-router.delete("/:id", deleteABook)
+router.delete("/:id",verifyAdminToken, deleteABook)
 
 
 module.exports = router;
